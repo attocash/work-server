57c2a8e4ce9524cc76d160ffbc85b820c1ca1a2c
rotilho <felipe@rotilho.com>
2024-07-25 18:56:04 +0200

    Use ConditionalOnMissingBean for worker creation

--- src/main/kotlin/cash/atto/work/ApplicationConfiguration.kt
+++ src/main/kotlin/cash/atto/work/ApplicationConfiguration.kt
@@ -3,12 +3,14 @@ package cash.atto.work
 import cash.atto.commons.AttoWorker
 import cash.atto.commons.opencl
 import cash.atto.work.adapter.ApplicationProperties
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class ApplicationConfiguration {
     @Bean
+    @ConditionalOnMissingBean
     fun openclWorker(properties: ApplicationProperties): List<AttoWorker> =
         (1..properties.queueSize)
             .map {
